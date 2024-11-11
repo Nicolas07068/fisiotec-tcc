2eb1844acbc4ab2b42471e1b9cf2b16a334bb578
Nicolas Cardoso <nicolacocao@gmail.com>
2024-11-11 12:26:07 +0000

    Olaaa

--- src/pages/Jogo.jsx
+++ src/pages/Jogo.jsx
@@ -7,7 +7,7 @@ export default function Jogo() {
         <div className='xl:max-w-7xl xl:m-auto'>
         <div className='xl:h-[640px] h-[450px]'>
             <h1 className='text-gradient bg-gradient-to-r from-verdinho via-azulzinho to-roxinho bg-clip-text text-transparent text-3xl font-bold text-center mt-10'>NOSSO JOGO</h1>
-            <iframe  className=" mt-10 w-[380px] ml-[50px] xl:w-11/12 xl:h-full xl:ml-[85px] h-[330px]" width="560" height="315" src="https://www.youtube.com/embed/XdBqoDMmDys?si=h7qHqdvIM6JmuWMn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe  className=" mt-10 w-[370px] ml-[30px] xl:w-11/12 xl:h-full xl:ml-[85px] h-[340px]" width="560" height="315" src="https://www.youtube.com/embed/XdBqoDMmDys?si=h7qHqdvIM6JmuWMn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           
         </div>
         </div>
